46cd18cafff83dcd73e3d09321e99241ea007339
Nikola Kosich <n.kosich@yandex.ru>
2022-07-26 16:19:51 +0300

    fixi

--- scripts/index.js
+++ scripts/index.js
@@ -1,15 +1,17 @@
 const popup = document.querySelector(".popup");
 const popupBtn = document.querySelector(".edit-button");
 const popupClose = document.querySelector (".popup-form__close-button");
-const nameInput = document.querySelector (".popup-form__input_text-name");
-const jobInput = document.querySelector (".popup-form__input_text-job");
+const nameInput = document.querySelector (".popup-form__input_text_name");
+const jobInput = document.querySelector (".popup-form__input_text_job");
 const you = document.querySelector (".profile__name");
 const job = document.querySelector (".profile__profession");
 function openPopup() {
-  popup.classList.add("popup-form__open");
+  nameInput.value = you.textContent ;
+  jobInput.value = job.textContent;
+  popup.classList.add("popup_open");
 }
 function closePopup() {
-  popup.classList.remove("popup-form__open");
+  popup.classList.remove("popup_open");
 }
 popupBtn.addEventListener("click", openPopup);
 
@@ -19,7 +21,7 @@ function formSubmitHandler (evt) {
   evt.preventDefault();
   you.textContent = nameInput.value;
   job.textContent = jobInput.value;
-  popup.classList.remove("popup-form__open");
+  closePopup();
 }
 
 popup.addEventListener('submit', formSubmitHandler);
